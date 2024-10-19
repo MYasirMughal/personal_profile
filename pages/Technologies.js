9b3d026331c42f3977827577111fbe38d484fe98
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-19 19:31:51 +0000

    Changed

--- pages/Technologies.js
+++ pages/Technologies.js
@@ -7,16 +7,13 @@ const Technologies = () => {
       <section>
         <h2 className="text-2xl font-semibold">👨‍💻 Programming languages</h2>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Programming-Languages/c.svg" alt="C language" height={50} width={50} />
-          <Image src="/Programming-Languages/c++.svg" alt="C++ language" height={50} width={50} />
-          <Image src="/Programming-Languages/python.svg" alt="python" height={50} width={50} />
-          <Image src="/Programming-Languages/java.svg" alt="java" height={50} width={50} />
-          <Image src="/Programming-Languages/c#.svg" alt="C#" height={50} width={50} />
-          <Image src="/Programming-Languages/bash.svg" alt="Bash" height={50} width={50} />
-          <Image src="/Programming-Languages/Batch.png" alt="Batch" height={50} width={50} />
-          <Image src="/Programming-Languages/PowerShell.png" alt="PowerShell" height={50} width={50} />
-          <Image src="/Programming-Languages/Solidity.png" alt="Solidity" height={50} width={50} />
-          <Image src="/Programming-Languages/rust.svg" alt="Rust" height={50} width={50} />
+          <Image src="/Logos/c.svg" alt="C language" height={50} width={50} />
+          <Image src="/Logos/c++.svg" alt="C++ language" height={50} width={50} />
+          <Image src="/Logos/python.svg" alt="Python" height={50} width={50} />
+          <Image src="/Logos/bash.svg" alt="Bash" height={50} width={50} />
+          <Image src="/Logos/Batch.png" alt="Batch" height={50} width={50} />
+          <Image src="/Logos/PowerShell.png" alt="PowerShell" height={50} width={50} />
+          <Image src="/Logos/Solidity.png" alt="Solidity" height={50} width={50} />
         </div>
       </section>
 
@@ -24,35 +21,35 @@ const Technologies = () => {
         <h2 className="text-2xl font-semibold">🌎 Web-Development</h2>
         <h3 className="text-xl">Front-End</h3>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Others/html.svg" alt="HTML" height={50} width={50} />
-          <Image src="/Others/css.svg" alt="CSS" height={50} width={50} />
-          <Image src="/Programming-Languages/javascript.svg" alt="JS" height={50} width={50} />
-          <Image src="/Programming-Languages/typescript.svg" alt="TypeScript" height={50} width={50} />
+          <Image src="/Logos/html.svg" alt="HTML" height={50} width={50} />
+          <Image src="/Logos/css.svg" alt="CSS" height={50} width={50} />
+          <Image src="/Logos/javascript.svg" alt="JS" height={50} width={50} />
+          <Image src="/Logos/typescript.svg" alt="TypeScript" height={50} width={50} />
         </div>
 
         <h3 className="text-xl mt-4">CSS Libraries</h3>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Frameworks/FrontEnd-Frameworks/Bootstrap.png" alt="BootstrapCSS" height={50} width={50} />
-          <Image src="/Frameworks/FrontEnd-Frameworks/TailwindCSS.png" alt="TailwindCSS" height={50} width={50} />
+          <Image src="/Logos/Bootstrap.png" alt="BootstrapCSS" height={50} width={50} />
+          <Image src="/Logos/TailwindCSS.png" alt="TailwindCSS" height={50} width={50} />
         </div>
       </section>
 
       <section>
         <h2 className="text-2xl font-semibold">Back-End</h2>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Programming-Languages/php.png" alt="php" height={50} width={50} />
-          <Image src="/Programming-Languages/javascript.svg" alt="JS" height={50} width={50} />
-          <Image src="/Frameworks/BackEnd-Frameworks/NodeJS.svg" alt="NodeJS" height={50} width={50} />
-          <Image src="/Frameworks/BackEnd-Frameworks/ASP.Net Web Application Development.png" alt="ASP.Net Web Development" height={50} width={50} />
+          <Image src="/Logos/php.png" alt="php" height={50} width={50} />
+          <Image src="/Logos/javascript.svg" alt="JS" height={50} width={50} />
+          <Image src="/Logos/NodeJS.svg" alt="NodeJS" height={50} width={50} />
+          
         </div>
       </section>
 
       <section>
         <h2 className="text-2xl font-semibold">Databases</h2>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Databases/mysql.svg" alt="MySQL" height={50} width={50} />
-          <Image src="/Databases/oracle.svg" alt="Oracle" height={50} width={50} />
-          <Image src="/Databases/postgresql.svg" alt="PostgreSQL" height={50} width={50} />
+          <Image src="/Logos/mysql.svg" alt="MySQL" height={50} width={50} />
+          <Image src="/Logos/oracle.svg" alt="Oracle" height={50} width={50} />
+          <Image src="/Logos/postgresql.svg" alt="PostgreSQL" height={50} width={50} />
         </div>
       </section>
     </div>
