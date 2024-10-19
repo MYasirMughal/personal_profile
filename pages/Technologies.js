93a085575585301cd9a541a9fbe3d6affb8076d3
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-19 20:04:12 +0000

    Change

--- pages/Technologies.js
+++ pages/Technologies.js
@@ -5,7 +5,11 @@ const Technologies = () => {
   return (
     <div className="p-6 space-y-8">
       <section>
-        <h2 className="text-2xl font-semibold">👨‍💻 Programming languages</h2>
+
+
+      <h1 className="pt-8 pb-4 text-3xl font-light">Programming Languages</h1>
+        
+      
         <div className="flex flex-wrap space-x-4 space-y-2">
           <Image src="/Logos/c.svg" alt="C language" height={50} width={50} />
           <Image src="/Logos/c++.svg" alt="C++ language" height={50} width={50} />
@@ -18,7 +22,9 @@ const Technologies = () => {
       </section>
 
       <section>
-        <h2 className="text-2xl font-semibold">🌎 Web-Development</h2>
+
+<h1 className="pt-8 pb-4 text-3xl font-light">Web Development</h1>
+        
         <h3 className="text-xl">Front-End</h3>
         <div className="flex flex-wrap space-x-4 space-y-2">
           <Image src="/Logos/html.svg" alt="HTML" height={50} width={50} />
@@ -29,27 +35,27 @@ const Technologies = () => {
 
         <h3 className="text-xl mt-4">CSS Libraries</h3>
         <div className="flex flex-wrap space-x-4 space-y-2">
-          <Image src="/Logos/Bootstrap.png" alt="BootstrapCSS" height={50} width={50} />
+          <Image src="/Logos/boostrap.svg" alt="BootstrapCSS" height={50} width={50} />
           <Image src="/Logos/TailwindCSS.png" alt="TailwindCSS" height={50} width={50} />
         </div>
       </section>
 
       <section>
-        <h2 className="text-2xl font-semibold">Back-End</h2>
+
+        <h1 className="pt-8 pb-4 text-3xl font-light">Back-End</h1>
         <div className="flex flex-wrap space-x-4 space-y-2">
           <Image src="/Logos/php.png" alt="php" height={50} width={50} />
           <Image src="/Logos/javascript.svg" alt="JS" height={50} width={50} />
-          <Image src="/Logos/NodeJS.svg" alt="NodeJS" height={50} width={50} />
+          <Image src="/Logos/nodejs.svg" alt="NodeJS" height={50} width={50} />
           
         </div>
       </section>
 
       <section>
-        <h2 className="text-2xl font-semibold">Databases</h2>
+       <h1 className="pt-8 pb-4 text-3xl font-light">Database</h1>       
         <div className="flex flex-wrap space-x-4 space-y-2">
           <Image src="/Logos/mysql.svg" alt="MySQL" height={50} width={50} />
-          <Image src="/Logos/oracle.svg" alt="Oracle" height={50} width={50} />
-          <Image src="/Logos/postgresql.svg" alt="PostgreSQL" height={50} width={50} />
+          
         </div>
       </section>
     </div>
