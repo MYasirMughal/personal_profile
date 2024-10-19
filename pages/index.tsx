02eb6c93e08f951448eaa0f45ede7a6378aa607d
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-19 11:27:05 -0700

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -162,6 +162,169 @@ const Home: NextPage = () => {
           </div>
  
 
+
+ {/* Programming */}
+
+## Progamming
+
+
+###  👨‍💻  Programming languages
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/c.svg" alt="C language" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/c++.svg" alt="C++ language" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/python.svg" alt="python" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/java.svg" alt="java" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/c#.svg" alt="C#" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/bash.svg" alt="Bash" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/Batch.png" alt="Batch" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/PowerShell.png" alt="PowerShell" height="50" width="50" /> 
+
+
+<br>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/Solidity.png" alt="Solidity" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/rust.svg" alt="Rust" height="50" width="50" /> 
+
+
+</p>
+
+### 🌎 Web-Development
+#### Front-End:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Others/html.svg" alt="HTML" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Others/css.svg" alt="CSS" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/javascript.svg" alt="JS" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/typescript.svg" alt="TypeScript" height="50" width="50" /> 
+</p>
+
+#### CSS Libraries:
+
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Bootstrap.png" alt="BootstrapCSS" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/TailwindCSS.png" alt="TailwindCSS" height="50" width="50" />
+</p>
+
+#### Tailwind CSS Components Libraries:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Tailwind-Components-Libraries/Shadcn.png" alt="Shadcn" height="50" width="50" />
+</p>
+
+#### Bootstrap CSS Components Libraries:
+
+#### ReactJS Libraries:
+#### ReactJS State Management Libraries:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Redux.svg" alt="Redux" height="50" width="50" />
+
+
+
+</p>
+
+#### ReactJS Data Fetching Libraries:
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Data-Fetching-Libraries/Axios.png" alt="Axios" height="50" width="50" />
+
+
+</p>
+
+#### ReactJS Headless CMS
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Headless-CMS/Sanity.io.png" alt="Sanity.io" height="50" width="50" />
+</p>
+
+
+#### ReactJS Form Handling Libraries:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Form-Handling-Libraries/Formspree.png" alt="Formspree" height="50" width="50" />
+</p>
+
+
+#### ReactJS UI Components Libraries:
+
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/UI-Components-Libraries/RTE.png" alt="React-Typewriter-Effect" height="50" width="50" />
+</p>
+
+#### ReactJS Motion Libraries:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/FrontEnd-Frameworks/Motion-Libraries/Framer-Motion.png" alt="Framer Motion" height="50" width="50" />
+</p>
+
+#### Back-End:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/php.png" alt="php" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Programming-Languages/javascript.svg" alt="JS" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/NodeJS.svg" alt="NodeJS" height="50" width="50" />
+
+<br>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/ASP.Net Web Application Development.png" alt="ASP.Net Web Development" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/Asp.net.png" alt="ASP.Net" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/ASP.Net Core.png" alt="ASP.Net Core" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/ASP.Net MVC.png" alt="ASP.Net MVC" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Frameworks/BackEnd-Frameworks/VB.Net.png" alt="VB.Net" height="50" width="50" />
+
+</p>
+
+## Databases
+
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/mysql.svg" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/oracle.svg" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/postgresql.svg" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/mongodb.svg" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/redis.svg" height="50" width="50" />
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Databases/cassandra.svg" height="50" width="50" />
+</p>
+
+
+
+## IDEs & Text Editors
+
+
+### IDEs:
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Text-Editors/vscode.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/IDEs/pycharm.svg" alt="python" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/IDEs/eclipse.svg" alt="java" height="50" width="50" /> 
+</p>
+
+### Text Editors:
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Text-Editors/atom.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Text-Editors/sublime.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Text-Editors/notepad++.png" height="50" width="50" /> 
+</p>
+
+## Cloud
+
+
+<p align='left'>
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/amazon.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/azure.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/gcloud.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/terraform.png" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/ansible.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/github.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/gitlab.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/heroku.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/firebase.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/docker.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/bitbucket.svg" height="50" width="50" /> 
+<img src="https://github.com/MYasirMughal/Logos/blob/main/Cloud/bitbucketV2.svg" height="50" width="50" /> 
+</p>
+
+
           {/* End */}
         </div>
       </main>
