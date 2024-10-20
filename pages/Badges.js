8528e72127a5a7de3afc8d3a83f14d285c343f2c
MYasirMughal <144555237+MYasirMughal@users.noreply.github.com>
2024-10-20 09:40:58 +0000

    Updated Badges.js

--- pages/Badges.js
+++ pages/Badges.js
@@ -10,9 +10,69 @@ const Badges = () => {
       
         <div className="flex flex-wrap space-x-4 space-y-2">
          
-          <Image src="/Logos/c.svg" alt="C language" height={50} width={50} />
+          <Image src="/Badges/AWS-SAA.png" alt="AWS Solution Architect" height={150} width={150} />
 
+
+
+            <Image src="/Badges/CCNA-ENSA.png" alt="CCNA ENSA" height={150} width={150} />
+
+
+
+            <Image src="/Badges/CCNA-SRWE.png" alt="CCNA SRWE" height={150} width={150} />
+
+
+            <Image src="/Badges/CCNA-ITN.png" alt="CCNA ITN" height={150} width={150} />
+
+
+
+            <Image src="/Badges/Network_Security.png" alt="Network Security" height={150} width={150} />
+
+
+
+
+            <Image src="/Badges/Networking-Essentials.png" alt="Networking Essentials" height={150} width={150} />
+
+
+                   
+          <Image src="/Badges/CyberOpsAssoc.png" alt="Cyber Ops Associate" height={150} width={150} />
+
+
+         
+          <Image src="/Badges/DEVASC.png" alt="DEV ASC" height={150} width={150} />
+
+
+                   
+          <Image src="/Badges/CyberEssentials.png" alt="CyberSecurity Essentials" height={150} width={150} />
+
+         
+          <Image src="/Badges/Networking-Basics.png" alt="Networking Basics" height={150} width={150} />
+
+         
+          <Image src="/Badges/ISC2.png" alt="ISC2" height={150} width={150} />
+
+         
+          <Image src="/Badges/ITCS.png" alt="ITCS" height={150} width={150} />
+          
+         
+          <Image src="/Badges/ITE.png" alt="ITE" height={150} width={150} />
+
+
+                   
+          <Image src="/Badges/CHB.png" alt="Computer Hardware Basics" height={150} width={150} />
+
+         
+          <Image src="/Badges/OSB.png" alt="Operting System Basics" height={150} width={150} />
+          
         </div>
+
+
+
+
+
+
+
+
+      
     </div>
 
       );
